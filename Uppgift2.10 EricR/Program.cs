646f87232f15cd52344a6e2318daadb4ab6ea5e5
EmilWallett <nhlpool27@gmail.com>
2022-09-07 12:05:48 +0200

    Add project files.

---
+++ Uppgift2.10 EricR/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift_2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine($"{bok} är en bra bok");
+        }
+    }
+}
